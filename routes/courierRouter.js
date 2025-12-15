3bf295e6c7f7f1489e5fa447756ac6471cbd6a59
alfininggara07-coder <alfininggara07@gmail.com>
2025-12-16 01:11:54 +0800

    Update courierRouter.js

--- routes/courierRouter.js
+++ routes/courierRouter.js
@@ -8,9 +8,9 @@ import {
 
 const courierRouter = express.Router();
 
-courierRouter.get("/api/courier/info", handleCourierInfo);
-courierRouter.post("/api/courier/login", handleCourierLogin);
-courierRouter.post("/api/courier/rating", handleCourierRating);
-courierRouter.get("/get/courier/statistik", handleCourierStatistik);
+courierRouter.get("/courier/info", handleCourierInfo);
+courierRouter.post("/courier/login", handleCourierLogin);
+courierRouter.post("/courier/rating", handleCourierRating);
+courierRouter.get("/courier/statistik", handleCourierStatistik);
 
 export default courierRouter;
