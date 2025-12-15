15acf3d6fccf1edd14d31b2734ccce01e1011b69
alfininggara07-coder <alfininggara07@gmail.com>
2025-12-16 01:15:10 +0800

    Update deliveryRouter.js

--- routes/deliveryRouter.js
+++ routes/deliveryRouter.js
@@ -12,11 +12,12 @@ import {
 const deliveryRouter = express.Router();
 
 deliveryRouter.post("/update/delivery", updateDelivery);
-deliveryRouter.get("/get/delivery", getRequestDelivery);
-deliveryRouter.get("/api/kip/deliverys", getKipDelivery);
+deliveryRouter.get("/delivery", getRequestDelivery);
+deliveryRouter.get("/kip/deliverys", getKipDelivery);
 deliveryRouter.post("/delivery/request", requestDelivery);
-deliveryRouter.get("/api/all/deliverys", getAllRequestDelivery);
-deliveryRouter.post("/api/delivery/cancel", handleDeliveryCancle);
+deliveryRouter.get("/all/deliverys", getAllRequestDelivery);
+deliveryRouter.post("/delivery/cancel", handleDeliveryCancle);
 deliveryRouter.get("/update/status/delivery", updateStatusDelivery);
 
 export default deliveryRouter;
+
